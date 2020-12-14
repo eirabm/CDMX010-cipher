871144658cb36da148ae27c05cae7326612fd53a
Eira Mazon <eira.b.mazon@gmail.com>
2020-12-14 11:26:02 -0600

    cpiher EM

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,43 @@
 const cipher = {
-  // ...
-};
+  // ...Aqui van las funciones 
+  encode: function(offset,texto) {
+    
+    var resultado = "";
+    
+    for(var i=0; i < texto.length; i++){
+      let char = texto.charCodeAt(i);
+  
+      if (char >= 65 && char <=  90){
+        document.getElementById("textoEncriptado").value = resultado += String.fromCharCode ((char - 65 + offset) % 26 + 65);}
+  
+      else if (char >= 97 && char <= 122){
+        document.getElementById("textoEncriptado").value = resultado += String.fromCharCode ((char - 97 + offset) % 26 + 97);}
+  
+      else {
+        document.getElementById("textoEncriptado").value = resultado += texto.charAt(i);}
+    }
+  },
+
+  decode: function (offset,textoEnc) {
+
+    var resultado = "";
+
+    var textoPrueba= document.getElementById("textoNormal");
+    textoPrueba.value="";
+  
+    for(var i=0; i < textoEnc.length; i++){
+      let char = textoEnc.charCodeAt(i);
+  
+       if (char >= 65 && char <=  90){
+        textoPrueba.value = resultado += String.fromCharCode ((char - 65 + offset) % 26 + 65);}
+  
+      else if (char >= 97 && char <= 122){
+        textoPrueba.value = resultado += String.fromCharCode ((char - 97 + offset) % 26 + 97);}
+  
+      else {
+        textoPrueba.value = resultado += textoEnc.charAt(i);}
+    }
+  } 
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let encodeButton = document.getElementById("encriptar");
+
+encodeButton.onclick = function(){
+
+    var texto = document.getElementById("textoNormal").value;
+
+    var desplazamiento = document.getElementById("ingresoOffset").value;
+    var offset = parseInt(desplazamiento); 
+
+    var encriptado = cipher.encode(offset,texto);
+}
+
+let decodeButton = document.getElementById("desencriptar");
+
+decodeButton.onclick = function(){
+
+    var textoEnc = document.getElementById("textoEncriptado").value;
+
+    var desplazamiento = document.getElementById("ingresoOffset").value;
+    var thisOffset = parseInt(desplazamiento); 
+    var offset = (26 - thisOffset) % 26;
+
+    var desencriptado = cipher.decode(offset,textoEnc)
+}
+
+document.getElementById("borrar").onclick = function(){
+    document.getElementById("textoNormal").value = ""
+}
+
+document.getElementById("copiar").onclick = function(){
+    var copiado = document.getElementById("textoEncriptado")
+    copiado.querySelector();
+    document.execCommand("copy");
+
+};
